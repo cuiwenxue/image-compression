a6bb85304ec27cc5d502709ed10112e1c77c8706
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2015-12-14 19:22:41 +0100

    Specifing path to neural network is now required

--- image_compression.py
+++ image_compression.py
@@ -15,7 +15,7 @@ def parse_arguments():
                             help='Create and teach new neural network using training data located in given directory')
     group_mode.add_argument('-c', '--compress', dest='image_path', type=str, help='Compress given image using existing neural network')
     group_mode.add_argument('-d', '--decompress', dest='image_path', type=str, help='Decompress given image using existing neural network')
-    parser.add_argument('-n', dest='neural_network', type=str, help='Loads/saves neural network from given path')
+    parser.add_argument('-n', dest='neural_network', type=str, help='Loads/saves neural network from given path', required=True)
     parser.add_argument('-D', '--debug', action='store_true', help='Run program in debug mode.')
     args = parser.parse_args()
     return args
