10775de3db51807b0e4cdf137976e2e4289980fb
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2015-12-14 19:16:02 +0100

    Neural network sturcture

---
+++ neural_network.py
@@ -0,0 +1,13 @@
+class NeuralNetwork():
+    """Neural network containing single hidden layer.
+       Teaching algorithm uses error backpropagation.
+    """
+    def __init__(self):
+        self.input_layer = []
+        self.hidden_layer = []
+        self.output_layer = []
+
+
+class Node():
+    def __init__(self):
+        self.value = 0
