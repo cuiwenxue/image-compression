b9d933fa82dff9e2c7b44ce75699b3257795eb2f
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2015-12-16 01:24:09 +0100

    Completed help and argument parsing

--- image_compression.py
+++ image_compression.py
@@ -1,40 +1,34 @@
 import argparse
 import logging
 import sys
-import random
-import compression
 import pickle
 
+import compression
 import neural_network
 
 
-def teach(repeat, learning_rate, neural_network_path):
+def teach(repeat, learning_rate, neural_network_path, learning_image):
     logging.getLogger('logger').info('Running program in teaching mode')
     network = neural_network.NeuralNetwork(64, [16], 64, learning_rate=learning_rate)
     network.init_weights()
     logging.getLogger('logger').info('Neural network edges initialized')
 
-    sys.stdout.write('Teaching in progress    ')
+    image = compression.openImage(learning_image)
+    sys.stdout.write('INFO: Teaching in progress    ')
     for i in xrange(repeat):
         sys.stdout.write('\b\b\b')
-        # TODO teaching algorithm implementation
-        data = compression.getRandomSquare(compression.openImage('59.bmp'))
+        data = compression.getRandomSquare(image)
         network.teach_step(data, data)
         sys.stdout.write("%d%%" % (100 * (i + 1) / repeat))
         sys.stdout.flush()
         sys.stdout.write('\n')
-
-    print "\n\tTEST RUN\n"
-    data = compression.getRandomSquare(compression.openImage('59.bmp'))
-    print data
-    print (network.run(data))
 
     logging.getLogger('logger').info('Teaching completed')
     neural_network.save(network, neural_network_path)
     logging.getLogger('logger').info('Neural network saved to ' + neural_network_path)
 
 
-def compress(image_path, neural_network_path):
+def compress(image_path, neural_network_path, compressed_image_path):
     logging.getLogger('logger').info('Running program in compression mode')
     try:
         network = neural_network.load(neural_network_path)
@@ -55,58 +49,84 @@ def compress(image_path, neural_network_path):
     pickle.dump(hidden, open('cmpIMG' + '.zdp', 'wb'), pickle.HIGHEST_PROTOCOL)
 
 
-def decompress(image_path, neural_network_path):
+def decompress(compressed_image_path, neural_network_path, target_image_path):
     logging.getLogger('logger').info('Running program in decompression mode')
     try:
         network = neural_network.load(neural_network_path)
     except neural_network.NeuralNetworkException as exc:
         logging.getLogger('logger').critical('Cannot load neural network: ' + exc.message)
-        exit(-1)
-        # TODO decompression algorithm
-    img = compression.openImage(image_path)
-    try:
-        hidden = pickle.load(open('cmpIMG.zdp', 'rb'))
-    except:
-        logging.getLogger('logger').critical('Cannot load compressed archive: ' + exc.message)
+        exit(1)
+    except IOError as exc:
+        logging.getLogger('logger').critical('Cannot load neural network: ' + exc.strerror)
+        exit(exc.errno)
 
+    # TODO decompression algorithm
+    raise NotImplementedError()
 
 
 def main():
     args = parse_arguments()
-    format_logger(args.debug)
+    format_logger()
+
+    if args.gui:
+        # TODO run in graphical mode
+        raise NotImplementedError()
     if args.teach:
-        # TODO get those parameters from argparse
-        teach(100000, 0.5, args.neural_network)
+        teach(args.teach[0], args.teach[1], args.teach[2], args.teach[3])
     elif args.compress:
-        compress(args.compress, args.neural_network)
+        compress(args.compress[0], args.compress[1], args.compress[2])
     else:
-        decompress(args.decompress, args.neural_network)
+        decompress(args.decompress[0], args.decompress[1], args.decompress[2])
+
+
+class TeachAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        repeat, rate, network, train = values
+        if not repeat.isdigit():
+            parser.error('REPEAT value must by positive integer')
+
+        if not 0 <= float(rate) <= 1:
+            parser.error('RATE value must [0.0; 1.0]')
+
+        setattr(namespace, self.dest, values)
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Image compression using neural network')
+    parser = argparse.ArgumentParser(description='Image compression using neural network', formatter_class=argparse.RawTextHelpFormatter)
     group_mode = parser.add_mutually_exclusive_group(required=True)
-    group_mode.add_argument('-t', '--teach', type=str,
-                            help='Create and teach new neural network using training data located in given directory')
-    group_mode.add_argument('-c', '--compress', type=str, help='Compress given image using existing neural network')
-    group_mode.add_argument('-d', '--decompress', type=str, help='Decompress given image using existing neural network')
-    parser.add_argument('-n', dest='neural_network', type=str, help='Loads/saves neural network from given path', required=True)
-    parser.add_argument('-D', '--debug', action='store_true', help='Run program in debug mode.')
+    group_mode.add_argument('-g', '--gui', action='store_true', help='Run program in graphical user interface')
+    group_mode.add_argument('-t', dest='teach', nargs=4, metavar=('REPEAT', 'RATE', 'NETWORK', 'TRAIN'), action=TeachAction,
+                            help='Create and teach new neural network using training da-\n'
+                                 'ta located in given directory\n\n'
+                                 'REPEAT   indicates how  many steps has to be taken dur-\n'
+                                 '         ing teaching algorithms\n'
+                                 'RATE     indicates learning rate, speed of teaching al-\n'
+                                 '         gorithm\n'
+                                 'NETWORK  indicates path where  generated neural network\n'
+                                 '         network will be saved\n'
+                                 'TRAIN    indicates path where is located training image\n')
+    group_mode.add_argument('-c', dest='compress', nargs=3, metavar=('IMAGE', 'NETWORK', 'COMPRESSED'),
+                            help='Compress given image using existing neural network\n\n'
+                                 'IMAGE    indicates path to compress image\n'
+                                 'NETWORK  indicates path where is located neural network\n'
+                                 'COMPRESSED indicates path  where compressed  image will\n'
+                                 '           be saved\n',)
+    group_mode.add_argument('-d', dest='decompress', nargs=3, metavar=('COMPRESSED', 'NETWORK', 'IMAGE'),
+                            help='Decompress given image using existing neural network\n\n'
+                                 'COMPRESSED indicates path to compressed image\n'
+                                 'NETWORK  indicates path where is located neural network\n'
+                                 'IMAGE    indicates path  where  decompressed image will\n'
+                                 '         be saved\n',)
     args = parser.parse_args()
     return args
 
 
-def format_logger(debug=False):
+def format_logger():
     logger = logging.getLogger('logger')
     console_handler = logging.StreamHandler()
     formatter = logging.Formatter('%(levelname)s: %(message)s')
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
-    if debug:
-        logger.setLevel(logging.DEBUG)
-        logging.getLogger('logger').info('Running program in debug mode')
-    else:
-        logger.setLevel(logging.INFO)
 
 
 if __name__ == '__main__':
