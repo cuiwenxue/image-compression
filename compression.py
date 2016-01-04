23ee8ec8fbd5306a047d75a387de3b04d6f6c0ff
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-01-04 18:28:08 +0100

    Added comments, refactorized

--- compression.py
+++ compression.py
@@ -7,28 +7,48 @@ from PIL import Image
 import neural_network
 
 
-def teach(neural_network_path, learning_image, repeat, learning_rate):
-    network = neural_network.NeuralNetwork(64, [32], 64, learning_rate=learning_rate)
+logger = logging.getLogger('logger')
+
+
+class ZdpException(Exception):
+    pass
+
+
+def teach(neural_network_path, learning_image, repeat, learning_rate, hidden_layer_size=32):
+    if repeat <= 0:
+        raise ZdpException('Number of repetitions must be grater than 0')
+    if not 0 <= learning_rate <= 1:
+        raise ZdpException('Learning rate must be <0;1>')
+    if not hidden_layer_size > 0 or hidden_layer_size % 8 != 0:
+        raise ZdpException('Hidden layer size must be multiple of 8')
+    network = neural_network.NeuralNetwork(64, [hidden_layer_size], 64, learning_rate=learning_rate)
     network.init_weights()
-    logging.getLogger('logger').info('Neural network edges initialized')
+    logger.info('Neural network edges initialized')
 
     image = Image.open(learning_image)
     for i in xrange(repeat):
-        data = get_random_square(image)
-        network.teach_step(data, data)
-        logging.getLogger('logger').info('Teaching in progress... %d%%\033[F' % (100 * (i + 1) / repeat))
+        x = random.randint(0, (image.size[0] - 8))
+        y = random.randint(0, (image.size[1] - 8))
+        colour = random.randint(0, 2)
+        data = get_square(image, x, y)
+        network.teach_step(data[colour], data[colour])
+        logger.info('Teaching in progress... %d%%\033[F' % (100 * (i + 1) / repeat))
 
-    logging.getLogger('logger').info('Teaching completed          ')
+    logger.info('Teaching completed          ')
     neural_network.save(network, neural_network_path)
-    logging.getLogger('logger').info('Neural network saved to ' + neural_network_path)
+    logger.info('Neural network saved to ' + neural_network_path)
 
 
 def compress(image_path, neural_network_path, compressed_image_path, bits):
+    if not 1 <= bits <= 8:
+        raise ZdpException('Number of bits must be <1;8>')
     network = neural_network.load(neural_network_path)
 
     img = Image.open(image_path)
     rgb_squares = get_sequence_squares(img)
+    size = bits * len(network.hidden_layers[0]) / 8
 
+    # open file and write data necessary to decompress
     file = open(compressed_image_path, 'wb')
     file.write(struct.pack('>i', img.size[0]))
     file.write(struct.pack('>i', img.size[1]))
@@ -37,15 +57,15 @@ def compress(image_path, neural_network_path, compressed_image_path, bits):
     file.write(struct.pack('>b', len(network.hidden_layers[0])))
 
     for i, rgb_square in enumerate(rgb_squares):
-        logging.getLogger('logger').info('Compressing in progress... %d%%\033[F' % (100 * (i + 1) / len(rgb_squares)))
+        logger.info('Compressing in progress... %d%%\033[F' % (100 * (i + 1) / len(rgb_squares)))
         for sq in rgb_square:
             network.run(sq)
+            # get hidden neuron's values
             hidden_values = [neuron.value for neuron in network.hidden_layers[0]]
             quant_values = quantify(hidden_values, bits)
 
-            size = bits * len(network.hidden_layers[0]) / 8
+            # place quantified values in byte array and write to file
             bin_square = bytearray([0] * size)
-
             B = 0
             pos = 0
             for val in quant_values:
@@ -58,31 +78,35 @@ def compress(image_path, neural_network_path, compressed_image_path, bits):
                     B += 1
                     pos %= 8
                     bin_square[B] |= val >> (bits - pos)
-
             file.write(bin_square)
+
     file.close()
-    logging.getLogger('logger').info('Compressing completed          ')
-    logging.getLogger('logger').info('Compressed image saved to ' + compressed_image_path)
+    logger.info('Compressing completed          ')
+    logger.info('Compressed image saved to ' + compressed_image_path)
 
 
 def decompress(compressed_image_path, neural_network_path, target_image_path):
-    logging.getLogger('logger').info('Compressing in progress...')
     network = neural_network.load(neural_network_path)
 
+    # read data necessary to decompress
     file = open(compressed_image_path, 'rb')
-
     x = struct.unpack('>i', file.read(4))[0]
     y = struct.unpack('>i', file.read(4))[0]
     number_of_rgb_squares = struct.unpack('>i', file.read(4))[0]
     bits = struct.unpack('>b', file.read(1))[0]
     hidden_layer_length = struct.unpack('>b', file.read(1))[0]
+
+    if hidden_layer_length != len(network.hidden_layers[0]):
+        raise ZdpException('Loaded network and compressed image are not compatible')
+
     size = bits * hidden_layer_length / 8
     mask = pow(2, bits) - 1
-
-    rgb_squares = []
+    squares = ([], [], [])
+    logger.info('Decompressing in progress...\033[F')
     for i in xrange(number_of_rgb_squares):
         rgb_square = ([], [], [])
 
+        # read 3 squares for each RGB colour
         for sq in rgb_square:
             B = 0
             pos = 0
@@ -99,61 +123,41 @@ def decompress(compressed_image_path, neural_network_path, target_image_path):
                     mask2 = pow(2, pos) - 1
                     val += (bin_square[B] & mask2) << (bits - pos)
                 sq.append(val)
-        rgb_squares.append(rgb_square)
 
-    squares = ([], [], [])
-    for rgb_square in rgb_squares:
+        # dequantify read squares, decompress and place in squares
         for i, sq in enumerate(rgb_square):
+            sq = dequantify(sq, bits)
             for j, val in enumerate(sq):
-                network.hidden_layers[0][j].value = float(val) / (pow(2, bits) - 1)
+                network.hidden_layers[0][j].value = val
             network.output_layer.update_values()
             output_values = [neuron.value for neuron in network.output_layer]
             squares[i].append(output_values)
 
     print_picture(Image.new('RGB', (x, y)), squares, target_image_path)
-    logging.getLogger('logger').info('Decompressing completed')
-    logging.getLogger('logger').info('Decompressed image saved to ' + target_image_path)
-
-
-def new_image(size):
-    return Image.new('F', size)
-
-
-def get_random_square(img):
-    """Get random square 8x8 from the grayscale picture
-       and quantify their colours to <0;1>
-    """
-    img_grayscale = img.convert('L')
-    x = random.randint(0, (img_grayscale.size[0] - 8))
-    y = random.randint(0, (img_grayscale.size[1] - 8))
-
-    square = []
-    for i in range(8):
-        for j in range(8):
-            square.append(img_grayscale.getpixel((x + j, y + i)))
-
-    return dequantify(square, 8)
+    logger.info('Decompressing completed     ')
+    logger.info('Decompressed image saved to ' + target_image_path)
 
 
 def get_sequence_squares(img):
-    """Get all consecutive squares from the picture
+    """Get all consecutive squares from the picture.
+       Pixel colour is converted to <0;1> value.
     """
     x, y = img.size
 
     squares = []
     for i in xrange(0, x, 8):
         for j in xrange(0, y, 8):
-            squares.append(get_fixed_square(img, i, j))
+            squares.append(get_square(img, i, j))
     return squares
 
 
-def get_fixed_square(img, x, y):
+def get_square(img, x, y):
     """Get tuple of 8x8 squares for each RGB colour
        from the fixed position of the picture
     """
     img_rgb = img.convert('RGB')
-    rgb_square = ([], [], [])
 
+    rgb_square = ([], [], [])
     for i in range(8):
         for j in range(8):
             r, g, b = img_rgb.getpixel((x + j, y + i))
@@ -165,8 +169,7 @@ def get_fixed_square(img, x, y):
 
 
 def quantify(values, bits):
-    """Quantify real number output of layer to the no. of bits
-    """
+    """Quantify real numbers to the no. of bits"""
     variants = pow(2, bits)
     step = 1.0 / variants
     quant = []
@@ -180,8 +183,7 @@ def quantify(values, bits):
 
 
 def dequantify(values, bits):
-    """Dequantifies list of values
-    """
+    """Dequantifies list of values - integer values to real values"""
     dequant = []
     for val in values:
         dequant.append(float(val) / (pow(2, bits) - 1))
@@ -189,25 +191,24 @@ def dequantify(values, bits):
     return dequant
 
 
-def print_picture(img, squares, name):
+def print_picture(img, squares, filename):
     """Print squares sequence into img
+       Saves under filename.
     """
     x, y = img.size
 
     idx = 0
     for i in xrange(0, x, 8):
         for j in xrange(0, y, 8):
-            put_square(img, i, j, (squares[0][idx], squares[1][idx], squares[2][idx]), name)
+            put_square(img, i, j, (squares[0][idx], squares[1][idx], squares[2][idx]))
             idx += 1
+    img.save(filename, 'BMP')
 
 
-def put_square(img, x, y, real_values, filename):
-    """Generate 8x8 square into given pos image from list of 64 realValues containing numbers 0.0-1.0
-       Saves under filename.
-    """
+def put_square(img, x, y, real_values):
+    """Generate 8x8 square into given pos image from list of 64 real_values containing numbers 0.0-1.0"""
     idx = 0
     for a in range(8):
         for b in range(8):
             img.putpixel((x + b, y + a), (int(real_values[0][idx] * 255), int(real_values[1][idx] * 255), int(real_values[2][idx] * 255)))
             idx += 1
-    img.save(filename, 'BMP')
